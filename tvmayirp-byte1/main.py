ead680d9fd5074d6ab941fd650fcfd28b43490e9
Priyamvada <priyamvadatiwari@Priyamvadas-MBP.wv.cc.cmu.edu>
2016-01-20 19:43:48 -0500

    Basic running with Flask

--- tvmayirp-byte1/main.py
+++ tvmayirp-byte1/main.py
@@ -15,11 +15,37 @@
 # limitations under the License.
 #
 import webapp2
+import os
+import jinja2
+import logging
+from flask import Flask
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = Flask(__name__)
+
+# class MainHandler(webapp2.RequestHandler):
+# 	def get(self):
+		# hello()
+		# self.response.write('Hello world!')
+		# self.hello()
+	# def hello():
+ #    	template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+ #    	return template.render()
+
+# class RenderHandler(webapp2.RequestHandler):
+# 	def post(self):
+# 		template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+#     	return template.render()
+
+@app.route('/')
+def hello():
+    template = JINJA_ENVIRONMENT.get_template('templates/index.html')
+    return template.render()
+
+# app = webapp2.WSGIApplication([
+# 	('/', MainHandler)
+# ], debug=True)
